be0566c6209d918cc2c24291973c62ed9615079b
dumbrosio <vd-ambro@student.42roma.it>
2023-11-17 17:20:30 +0100

    Add e_token enum. Format document.

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,30 +1,45 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
+# include "../libft/libft.h"
+# include <errno.h>
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
 # include <string.h>
-# include <errno.h>
-# include <sys/wait.h>
 # include <sys/types.h>
-# include <fcntl.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <signal.h>
-# include "../libft/libft.h"
+# include <sys/wait.h>
+# include <unistd.h>
 
 extern int	g_var;
 
-typedef struct s_shell {
+typedef struct s_shell
+{
 	char	*command;
 	char	*prompt;
-}	t_shell;
+}			t_shell;
+
+typedef enum e_token
+{
+	T_WORD,
+	T_PIPE,
+	T_LESS,
+	T_GREAT,
+	T_DLESS,
+	T_DGREAT,
+	T_NL,
+	T_EOF,
+	T_ERROR,
+	T_NOTOKEN
+}			t_token;
 
 /* minishell.c */
-void	init_shell(t_shell *shell);
-void	run_shell(t_shell *shell);
-void	clean_shell(t_shell *shell);
+void		init_shell(t_shell *shell);
+void		run_shell(t_shell *shell);
+void		clean_shell(t_shell *shell);
 /*signal*/
-void	signal_handler(int sig);
+void		signal_handler(int sig);
 
 #endif
